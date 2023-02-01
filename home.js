d7007ecb72c7c87346335383af7cfdaf477ea69a
AumPapimol <114016913+AumPapimol@users.noreply.github.com>
2023-02-01 15:37:57 +0700

    Add files via upload

---
+++ home.js
@@ -0,0 +1,83 @@
+// header
+
+// window.onscroll = function () { myFunction() };
+
+// var navbar = document.getElementById("navbar");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//     if (window.pageYOffset >= sticky) {
+//         navbar.classList.add("sticky")
+//     } else {
+//         navbar.classList.remove("sticky");
+//     }
+// }
+
+function openCity(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+    dropdown[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var dropdownContent = this.nextElementSibling;
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none";
+        } else {
+            dropdownContent.style.display = "block";
+        }
+    });
+}
+
+// IMG
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
+
+const getflashsale=()=>{
+    fetch('http://localhost:3111/promotion',{method:'GET',mode:'cors'})
+    .then((res)=>res.json())
+    .then((data)=>{
+        data.map((element)=>{
+        let createIMG = document.createElement("IMG")
+        let createA = document.createElement('a')
+        let imgDiv = document.getElementById('flashsale')
+        createIMG.setAttribute('src',element.image)
+        createIMG.classList.add('flashIMG')
+        // createA.setAttribute('href','/picview.html?id='+element.pid)
+        imgDiv.appendChild(createA)
+        createA.appendChild(createIMG)
+        })
+    })
+}
